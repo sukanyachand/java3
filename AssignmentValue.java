be8759938f766cdaf58534264d45694668038089
sukanyachand <sukanya9791@gmail.com>
2016-08-06 20:21:26 +0530

    Session1 Assignment3
    Session1 Assignment3

---
+++ AssignmentValue.java
@@ -0,0 +1,16 @@
+
+package sumnumber;
+
+public class AssignmentValue {
+
+    public static void main(String[] args) {
+    // session1 assignment 3 to check data that cannt be stored in int variable
+    int num1 = 0;
+    int num2 = 0;
+    boolean leap = true;
+    
+    num1 = "hello";
+    num2 = leap;
+    }
+    
+}
